d2c026e28946736376639c70d810c6e7f85cb947
Hendrew Hélio <hendrew.dev@gmail.com>
2024-10-19 09:26:27 -0300

    chore: add base layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def flash_message_class(level)
+    alerts = {
+      notice: "success",
+      alert: "danger",
+      error: "danger"
+    }
+
+    alerts[level.to_sym]
+  end
 end
